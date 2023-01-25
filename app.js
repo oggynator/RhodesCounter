909a8bcf92e743c4206a0bdec38acf68a7c4cdef
oggy <43633238+oggynator@users.noreply.github.com>
2023-01-25 15:27:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const html = `
       }, 500);
 
       // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date("Jul 24, 2023 06:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -81,9 +81,10 @@ var x = setInterval(function() {
   </head>
   <body>
     <section>
-      Hello from Render!
+      Dage til Druk Eksil hændelsen 2023!
+      <p id="demo"></p>
     </section>
-    <p id="demo"></p>
+    
   </body>
 </html>
 `
